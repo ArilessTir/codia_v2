960abec50e12bb35fb1543f21b09db64bf981e5d
ArilessTir <ariless.tir@gmail.com>
2022-11-11 20:45:04 +0100

    create homepage

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="bg-slate-400 h-[100vh] flex flex-wrap">
-      <h1 className="text-stone-900"> CODIA </h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/pages/homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <section className="">
+      <div>
+        <h1> Home page </h1>
+      </div>
+    </section>
+  );
+};
+
+export default Homepage;
